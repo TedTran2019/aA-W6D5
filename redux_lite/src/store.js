e6354609fc264daaba0d11db91e5fa5f1e0269e5
Ted Tran <TedTran2019@gmail.com>
2019-12-29 23:34:08 -0800

    Going to return tomorrow on Dec 30th. Dec 29th I just reread my notes to review

---
+++ redux_lite/src/store.js
@@ -0,0 +1,16 @@
+class Store {
+	constructor(rootReducer) {
+		this.rootReducer = rootReducer;
+		this.state = {};
+
+		this.getState = this.getState.bind(this);
+	}
+
+	// Sends in a copy of the state, so it can't be mutated through this
+	getState() {
+		return Object.assign({}, this.state);
+	}
+}
+
+const createStore = (...args) => new Store(...args);
+
